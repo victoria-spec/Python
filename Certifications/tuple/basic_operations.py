029fe5138c901b3bf93e9dac6e0a155de3d11f4b
victoria-spec <74970120+victoria-spec@users.noreply.github.com>
2024-10-24 19:04:11 -0400

    Add files via upload

---
+++ Certifications/tuple/basic_operations.py
@@ -0,0 +1,23 @@
+
+#mutable
+countries =("brazil",
+            "Canada",
+            "denmark",
+        )
+
+#countries =("girl",1)
+#print(countries)
+#countries[1] ="USA" #error
+
+#countries ="brazil", "Canada","denmark",
+#print(type(countries))        
+
+tuple =(1920,1080)
+#print(type(tuple))   
+
+#creating new tuple
+print(len(tuple))   
+#print(tuple[1:]) 
+#print(tuple[1])  
+
+
---
+++ Certifications/tuple/concatinating.py
@@ -0,0 +1,27 @@
+
+#tuple =(1920,1080)
+
+#tuple2 =(24,)
+
+#tuple3 =tuple+ tuple2 
+#print(type(tuple3)) 
+
+#tupe_num=tuple * 4
+#print(tupe_num) 
+
+tuple =(1920,1080)
+list_resolution =list(tuple)
+list_resolution.append(3000)
+print(type(list_resolution))
+
+my_list = [1, 2, 3, 4, 5]  # This is a list
+my_list = ['apple', 'banana', 'cherry', 'date']
+
+for index, item in enumerate(my_list):
+    print(f"Position: {index}, Item: {item}")
+
+
+
+#print(tuple2*4)
+
+#print(type(4)) 
